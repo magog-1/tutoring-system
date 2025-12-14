6c57f6b720d4999aa92fb6a34620b1c2ad6cf123
maksim_m <142458092+magog-1@users.noreply.github.com>
2025-12-14 23:22:01 +0300

    Add getters and setters to Subject class

--- src/main/java/com/tutoring/model/Subject.java
+++ src/main/java/com/tutoring/model/Subject.java
@@ -24,8 +24,57 @@ public class Subject {
     @ManyToMany(mappedBy = "subjects")
     private List<Tutor> tutors = new ArrayList<>();
 
-    // Конструкторы, геттеры, сеттеры
+    // Конструкторы
+    public Subject() {}
 
+    public Subject(String name, String description, SubjectCategory category) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+    }
+
+    // Геттеры и сеттеры
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public SubjectCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(SubjectCategory category) {
+        this.category = category;
+    }
+
+    public List<Tutor> getTutors() {
+        return tutors;
+    }
+
+    public void setTutors(List<Tutor> tutors) {
+        this.tutors = tutors;
+    }
+
+    // Enum
     public enum SubjectCategory {
         MATHEMATICS, LANGUAGES, SCIENCES,
         ARTS, PROGRAMMING, OTHER
