908b22dddf718a02a9ef6daf523d569eed10dfca
maksim_m <142458092+magog-1@users.noreply.github.com>
2025-12-15 21:38:48 +0300

    Add findByUsername method to StudentRepository

--- src/main/java/com/tutoring/repository/StudentRepository.java
+++ src/main/java/com/tutoring/repository/StudentRepository.java
@@ -12,6 +12,8 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
     Optional<Student> findByEmail(String email);
 
+    Optional<Student> findByUsername(String username);
+
     List<Student> findByEducationLevel(Student.EducationLevel level);
 
     @Query("SELECT s FROM Student s WHERE s.isActive = true")
